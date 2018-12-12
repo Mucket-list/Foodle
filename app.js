ad03457f9d23da02527d0d16ddcf5bcfc9cfd557
slee <home@slees-MacBook-Air.local>
2018-12-12 17:00:51 -0500

    update app.js

--- app.js
+++ app.js
@@ -87,8 +87,13 @@ app.use('/place/:id', places);
 app.use('/category', categories);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+// app.set('port', (process.env.PORT || 3000));
+//
+// app.listen(app.get('port'), function(){
+// 	console.log('Server started on port '+app.get('port'));
+// });
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Foodle Server Started...");
 });
